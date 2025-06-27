50ff68cdaaef3c31bc8b6c7c07d22c3467717943
Brayan Ruano <brc910tec@gmail.com>
2025-06-26 20:08:44 -0500

    Primera versión del backend

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import conectarDB from './config/db.js'
+
+import authRoutes from './routes/auth.js'
+import citasRoutes from './routes/citas.js'
+import insumosRoutes from './routes/insumos.js'
+import historiasRoutes from './routes/historias.js'
+
+dotenv.config()
+conectarDB()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+// Rutas
+app.use('/api/auth', authRoutes)
+app.use('/api/citas', citasRoutes)
+app.use('/api/insumos', insumosRoutes)
+app.use('/api/historias', historiasRoutes)
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`))
